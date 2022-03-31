f8299d73f8be2189fff47496a2f0667683fd2893
kemo99 <karim.ali@live.com>
2022-03-31 12:10:19 +0100

    athlete controllers and updated login

--- src/components/App.js
+++ src/components/App.js
@@ -11,27 +11,30 @@ import ArticleForm from './form/ArticleForm';
 import ManageHome from './manage/managehome.js';
 import ArticlesTable from './manage/article-manager/ArticlesTable';
 import Login from './auth/login';
-import Register from './auth/Register';
+// import Register from './auth/Register';
 import Navbar from './Navbar';
+import AthleteTable from './manage/athlete-manager/athletesTable';
+import NewAthleteForm from './athletes/NewAthleteForm';
+import AthleteManager from './manage/athlete-manager/AthleteManagerProfile';
 
 const App = () => (
   <BrowserRouter>
     <Navbar />
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/athletes' element={<Athletes />} />
-      <Route path='/athletes/:id' element={<SingleAthlete />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/discover' element={<Articles />} />
-      <Route path='/discover/:id' element={<SingleArticle />} />
-      <Route path='/manage' element={<ManageHome />} />
-      <Route path='/manage/newarticle' element={<ArticleForm />} />
-      <Route
-        path='/manage/articles/status/:status'
-        element={<ArticlesTable />}
-      />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/athletes" element={<Athletes />} />
+      <Route path="/athletes/:id" element={<SingleAthlete />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/discover" element={<Articles />} />
+      <Route path="/discover/:id" element={<SingleArticle />} />
+      <Route path="/manage" element={<ManageHome />} />
+      <Route path="/manage/newarticle" element={<ArticleForm />} />
+      <Route path="/manage/athletes/stat/:status" element={<AthleteTable />} />
+      <Route path="/manage/articles/stat/:status" element={<ArticlesTable />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/newAthlete" element={<NewAthleteForm />} />
+      <Route path="/manage/athlete/:athleteId" element={<AthleteManager />} />
+      {/* <Route path="/register" element={<Register />} /> */}
     </Routes>
   </BrowserRouter>
 );
